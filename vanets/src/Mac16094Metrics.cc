ba9bf0fd7cdd78af391c1715d48fdfd733204dda
Susana Rodriguez <surod22@gmail.com>
2018-09-11 18:39:26 +0000

    V-001: [Susana] Recording previousSignalQuality and packets per channel

--- vanets/src/Mac16094Metrics.cc
+++ vanets/src/Mac16094Metrics.cc
@@ -49,6 +49,20 @@ void Mac16094Metrics::initialize(int i) {
     channelUtilization.insert(std::pair<int, double>(Channels::SCH4, 0.0));
     channelUtilization.insert(std::pair<int, double>(Channels::HPPS, 0.0));
 
+    previousSignalQuality.insert(std::pair<int, double>(Channels::CCH, 0.0));
+    previousSignalQuality.insert(std::pair<int, double>(Channels::SCH1, 0.0));
+    previousSignalQuality.insert(std::pair<int, double>(Channels::SCH2, 0.0));
+    previousSignalQuality.insert(std::pair<int, double>(Channels::SCH3, 0.0));
+    previousSignalQuality.insert(std::pair<int, double>(Channels::SCH4, 0.0));
+
+    channelPacketsTransmitted.insert(std::pair<int, double>(Channels::CCH, 0.0));
+    channelPacketsTransmitted.insert(std::pair<int, double>(Channels::SCH1, 0.0));
+    channelPacketsTransmitted.insert(std::pair<int, double>(Channels::SCH2, 0.0));
+    channelPacketsTransmitted.insert(std::pair<int, double>(Channels::SCH3, 0.0));
+    channelPacketsTransmitted.insert(std::pair<int, double>(Channels::SCH4, 0.0));
+
+
+
     Mac1609_4::initialize(i);
 
     WATCH(throughputMetricMac);
@@ -61,6 +75,16 @@ void Mac16094Metrics::recordChannelUtilized(const simtime_t& sendingDuration) {
     int channelUtilized = (activeChannel == type_CCH) ? Channels::CCH : mySCH;
     channelUtilization[channelUtilized] = channelUtilization[channelUtilized]
             + sendingDuration;
+}
+
+void Mac16094Metrics::recordSignalQuality(double txPower){
+    int channelUtilized = (activeChannel == type_CCH) ? Channels::CCH : mySCH;
+    previousSignalQuality[activeChannel] = txPower;
+}
+
+void Mac16094Metrics::recordChannelPackets(int chan){
+    int channelNow = (activeChannel == type_CCH) ? Channels::CCH : mySCH;
+    channelPacketsTransmitted[channelNow] ++;
 }
 
 void Mac16094Metrics::handleSelfMsg(cMessage* msg) {
@@ -149,12 +173,14 @@ void Mac16094Metrics::handleSelfMsg(cMessage* msg) {
 
         simtime_t sendingDuration = RADIODELAY_11P + getFrameDuration(mac->getBitLength(), mcs);
         DBG_MAC << "Sending duration will be" << sendingDuration << std::endl;
+        std::cout<<"SendingDuration"<<sendingDuration<<std::endl;
         if ((!useSCH) || (timeLeftInSlot() > sendingDuration)) {
             if (useSCH) DBG_MAC << " Time in this slot left: " << timeLeftInSlot() << std::endl;
 
             double freq = (activeChannel == type_CCH) ? frequency[Channels::CCH] : frequency[mySCH];
 
             DBG_MAC << "Sending a Packet. Frequency " << freq << " Priority" << lastAC << std::endl;
+
             sendFrame(mac, RADIODELAY_11P, freq, datarate, txPower_mW);
 
             if (pktToSend->getRecipientAddress() != -1 && useAcks) {
@@ -170,6 +196,7 @@ void Mac16094Metrics::handleSelfMsg(cMessage* msg) {
             }
 
             recordChannelUtilized(sendingDuration);
+            recordSignalQuality(txPower_mW);
 
         } else {   //not enough time left now
             DBG_MAC<< "Too little Time left. This packet cannot be send in this slot."<< std::endl;
@@ -194,13 +221,26 @@ void Mac16094Metrics::finish() {
     recordScalar("packetsNotForMe", packetsNotForMe);
     recordScalar("receivedTotalBits", statsReceivedBits);
     recordScalar("collisionsPktNonDecoded", collisionsPktNonDecoded);
+
+    recordScalar("chUtilizationCCH", channelUtilization.at(Channels::CCH));
     recordScalar("chUtilizationSCH1", channelUtilization.at(Channels::SCH1));
     recordScalar("chUtilizationSCH2", channelUtilization.at(Channels::SCH2));
     recordScalar("chUtilizationSCH3", channelUtilization.at(Channels::SCH3));
     recordScalar("chUtilizationSCH4", channelUtilization.at(Channels::SCH4));
-    recordScalar("chUtilizationCCH", channelUtilization.at(Channels::CCH));
     recordScalar("chUtilizationHPPS", channelUtilization.at(Channels::HPPS));
     recordScalar("chUtilizationCRIT_SOL",channelUtilization.at(Channels::CRIT_SOL));
+
+    recordScalar("previousSignalQualityCCH", channelUtilization.at(Channels::CCH));
+    recordScalar("previousSignalQualitySCH1", channelUtilization.at(Channels::SCH1));
+    recordScalar("previousSignalQualitySCH2", channelUtilization.at(Channels::SCH2));
+    recordScalar("previousSignalQualitySCH3", channelUtilization.at(Channels::SCH3));
+    recordScalar("previousSignalQualitySCH4", channelUtilization.at(Channels::SCH4));
+
+    recordScalar("chPacketsCCH", channelPacketsTransmitted.at(Channels::CCH));
+    recordScalar("chPacketsSCH1", channelPacketsTransmitted.at(Channels::SCH1));
+    recordScalar("chPacketsSCH2", channelPacketsTransmitted.at(Channels::SCH2));
+    recordScalar("chPacketsSCH3", channelPacketsTransmitted.at(Channels::SCH3));
+    recordScalar("chPacketsSCH4", channelPacketsTransmitted.at(Channels::SCH4));
 
     Mac1609_4::finish();
 }
@@ -269,9 +309,80 @@ void Mac16094Metrics::handleUpperControl(cMessage* message) {
     Mac1609_4::handleUpperControl(message);
 }
 
-void Mac16094Metrics::handleUpperMsg(cMessage* message) {
+void Mac16094Metrics::handleUpperMsg(cMessage* msg) {
 
-       Mac1609_4::handleUpperMsg(message);
+    WaveShortMessage* thisMsg;
+    if ((thisMsg = dynamic_cast<WaveShortMessage*>(msg)) == NULL) {
+        error("WaveMac only accepts WaveShortMessages");
+    }
+
+    t_access_category ac = mapUserPriority(thisMsg->getUserPriority());
+
+    DBG_MAC << "Received a message from upper layer for channel "
+            << thisMsg->getChannelNumber() << " Access Category (Priority):  "
+            << ac << std::endl;
+
+    t_channel chan;
+
+    if (thisMsg->getChannelNumber() == Channels::CCH) {
+        chan = type_CCH;
+    }
+    else {
+        ASSERT(useSCH);
+        thisMsg->setChannelNumber(mySCH);
+        chan = type_SCH;
+    }
+
+    int num = myEDCA[chan]->queuePacket(ac,thisMsg);
+
+    //packet was dropped in Mac
+    if (num == -1) {
+        statsDroppedPackets++;
+        return;
+    }
+
+    //if this packet is not at the front of a new queue we dont have to reevaluate times
+    DBG_MAC << "sorted packet into queue of EDCA " << chan << " this packet is now at position: " << num << std::endl;
+
+    if (chan == activeChannel) {
+        DBG_MAC << "this packet is for the currently active channel" << std::endl;
+        recordChannelPackets(chan);
+
+    }
+    else {
+        DBG_MAC << "this packet is NOT for the currently active channel" << std::endl;
+    }
+
+    if (num == 1 && idleChannel == true && chan == activeChannel) {
+
+        WaveShortMessage* someMsg = dynamic_cast<WaveShortMessage*>(msg);
+        simtime_t duration = someMsg->getDuration();
+        std::cout<<"Duration time in active channel" << duration <<std::endl;
+        simtime_t nextEvent = myEDCA[chan]->startContent(lastIdle,guardActive());
+
+        if (nextEvent != -1) {
+            if ((!useSCH) || (nextEvent <= nextChannelSwitch->getArrivalTime())) {
+                if (nextMacEvent->isScheduled()) {
+                    cancelEvent(nextMacEvent);
+                }
+                scheduleAt(nextEvent,nextMacEvent);
+                DBG_MAC << "Updated nextMacEvent:" << nextMacEvent->getArrivalTime().raw() << std::endl;
+            }
+            else {
+                DBG_MAC << "Too little time in this interval. Will not schedule nextMacEvent" << std::endl;
+                //it is possible that this queue has an txop. we have to revoke it
+                myEDCA[activeChannel]->revokeTxOPs();
+                statsNumTooLittleTime++;
+            }
+        }
+        else {
+            cancelEvent(nextMacEvent);
+        }
+    }
+    if (num == 1 && idleChannel == false && myEDCA[chan]->myQueues[ac].currentBackoff == 0 && chan == activeChannel) {
+        myEDCA[chan]->backoff(ac);
+    }
+
 }
 
 int Mac16094Metrics::randomizeSCH( int min, int max) {
@@ -292,11 +403,105 @@ int Mac16094Metrics::randomizeSCH( int min, int max) {
 
 
 
-void Mac16094Metrics::handleLowerControl(cMessage* msg) {
-    Mac1609_4::handleLowerControl(msg);
+void Mac1609_4::handleLowerControl(cMessage* msg) {
+    if (msg->getKind() == MacToPhyInterface::PHY_RX_START) {
+        rxStartIndication = true;
+    } else if (msg->getKind() == MacToPhyInterface::PHY_RX_END_WITH_SUCCESS) {
+        // PHY_RX_END_WITH_SUCCESS will get packet soon! Nothing to do here
+    } else if (msg->getKind() == MacToPhyInterface::PHY_RX_END_WITH_FAILURE) {
+        // RX failed at phy. Time to retransmit
+        phy11p->notifyMacAboutRxStart(false);
+        rxStartIndication = false;
+        handleRetransmit(lastAC);
+    } else if (msg->getKind() == MacToPhyInterface::TX_OVER) {
 
+        DBG_MAC << "Successfully transmitted a packet on " << lastAC << std::endl;
+
+        phy->setRadioState(Radio::RX);
+
+        if (!dynamic_cast<Mac80211Ack*>(lastMac.get())) {
+            //message was sent
+            //update EDCA queue. go into post-transmit backoff and set cwCur to cwMin
+            myEDCA[activeChannel]->postTransmit(lastAC, lastWSM, useAcks);
+        }
+        //channel just turned idle.
+        //don't set the chan to idle. the PHY layer decides, not us.
+
+        if (guardActive()) {
+            throw cRuntimeError("We shouldnt have sent a packet in guard!");
+        }
+    }
+    else if (msg->getKind() == Mac80211pToPhy11pInterface::CHANNEL_BUSY) {
+        channelBusy();
+    }
+    else if (msg->getKind() == Mac80211pToPhy11pInterface::CHANNEL_IDLE) {
+        // Decider80211p::processSignalEnd() sends up the received packet to MAC followed by control message CHANNEL_IDLE in the same timestamp.
+        // If we received a unicast frame (first event scheduled by Decider), MAC immediately schedules an ACK message and wants to switch the radio to TX mode.
+        // So, the notification for channel idle from phy is undesirable and we skip it here.
+        // After ACK TX is over, PHY will inform the channel status again.
+        if (ignoreChannelState) {
+            // Skipping channelidle because we are about to send an ack regardless of the channel state
+        } else {
+            channelIdle();
+        }
+    }
+    else if (msg->getKind() == Decider80211p::BITERROR || msg->getKind() == Decider80211p::COLLISION) {
+        statsSNIRLostPackets++;
+        DBG_MAC << "A packet was not received due to biterrors" << std::endl;
+    }
+    else if (msg->getKind() == Decider80211p::RECWHILESEND) {
+        statsTXRXLostPackets++;
+        DBG_MAC << "A packet was not received because we were sending while receiving" << std::endl;
+    }
+    else if (msg->getKind() == MacToPhyInterface::RADIO_SWITCHING_OVER) {
+        DBG_MAC << "Phylayer said radio switching is done" << std::endl;
+    }
+    else if (msg->getKind() == BaseDecider::PACKET_DROPPED) {
+        phy->setRadioState(Radio::RX);
+        DBG_MAC << "Phylayer said packet was dropped" << std::endl;
+    }
+    else {
+        DBG_MAC << "Invalid control message type (type=NOTHING) : name=" << msg->getName() << " modulesrc=" << msg->getSenderModule()->getFullPath() << "." << std::endl;
+        assert(false);
+    }
+
+    if (msg->getKind() == Decider80211p::COLLISION) {
+        emit(sigCollision, true);
+    }
+
+    delete msg;
 }
 
+void Mac1609_4::sendFrame(Mac80211Pkt* frame, simtime_t delay, double frequency, uint64_t datarate, double txPower_mW) {
+    phy->setRadioState(Radio::TX); // give time for the radio to be in Tx state before transmitting
+
+    delay = std::max(delay, RADIODELAY_11P); // wait at least for the radio to switch
+
+    attachSignal(frame, simTime() + delay, frequency, datarate, txPower_mW);
+    MacToPhyControlInfo* phyInfo = dynamic_cast<MacToPhyControlInfo*>(frame->getControlInfo());
+    ASSERT(phyInfo);
+
+    lastMac.reset(frame->dup());
+    sendDelayed(frame, delay, lowerLayerOut);
+
+    if (dynamic_cast<Mac80211Ack*>(frame)) {
+        statsSentAcks += 1;
+    } else {
+        statsSentPackets += 1;
+    }
+}
+
+
+void Mac1609_4::attachSignal(Mac80211Pkt* mac, simtime_t startTime, double frequency, uint64_t datarate, double txPower_mW) {
+
+    simtime_t duration = getFrameDuration(mac->getBitLength());
+
+    Signal* s = createSignal(startTime, duration, txPower_mW, datarate, frequency);
+
+    MacToPhyControlInfo* cinfo = new MacToPhyControlInfo(s);
+
+    mac->setControlInfo(cinfo);
+}
 
 
 void Mac16094Metrics::computeThroughput(Metrics* metrics,
